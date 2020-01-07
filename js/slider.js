4e673843073596e25726f2051306161e9973cf9c
Killoxe <polinasima2@ya.ru>
2020-01-07 23:43:48 +0500

    adaptiv phone and laptop / доработаны слайдеры, добавлен адаптив

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.slider__wrap', {
         loop: true,
         autoplay: {
             delay: 6000,
             disableOnInteraction: false,
         },
-        spaceBetween: 100,
-        effect: 'coverflow',
+        speed: 1500,
+        spaceBetween: 50,
         pagination: {
             el: '.swiper-pagination',
             type: 'fraction',
@@ -15,8 +15,35 @@ $(document).ready(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        // fadeEffect: {
-        //     crossFade: true
-        // },
+        effect: 'coverflow',
+        coverflowEffect: {
+            rotate: 90,
+            slideShadows: false,
+        },
     });
+    var swiper = new Swiper('.aboutUs__slider', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        breakpoints: {
+            1601: {
+                slidesPerView: 5,
+            },
+            1367: {
+                slidesPerView: 4,
+            },
+            769: {
+                slidesPerView: 3,
+            },
+            481: {
+                slidesPerView: 2,
+            },
+
+        }
+    });
+  
 });
